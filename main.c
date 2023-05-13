b7e4b26afbf5a97066bb7c6deb670be71589c45f
Pedro Vinnícius Bernhard <pedro.bernhard@nca.ufma.br>
2023-05-13 03:51:56 -0300

    add: get_pi, quantidade_requisicoes

--- main.c
+++ main.c
@@ -1,19 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-int main(int argc, char const *argv[]);
+// valor de PI com precisão de 100 casas decimais
+const char PI[] = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+
+int main(int, char **);
 int get_aleatorio(int, int);
+char *get_pi(char *, int);
+void gera_requisicoes(char *, int, int, int, int, int);
 
-int main(int argc, char const *argv[])
+int main(int argc, char **argv)
 {
-  // inicializa seed de números aleatório com o tempo atual
-  srand(time(NULL));
+  // número de threads trabalhadoras
+  const int N = 10;
 
-  for (int i = 0; i < 10; i++)
+  // periodicidade das requisições em milisegundos
+  const int TEMPOREQ = 250;
+
+  int quantidade_requisicoes = 0;
+
+  // checa se foi passado argumentos para o programa
+  if (argc > 1)
   {
-    printf("%d: %d\n", i, get_aleatorio(6, 9));
+    // lê o primeiro argumento e avisa caso seja inválido
+    if (sscanf(argv[1], "%d", &quantidade_requisicoes) != 1)
+    {
+      puts("Argumento inválido.");
+    }
   }
+  else
+  {
+    puts("Nenhum argumento foi passado ao programa.");
+  }
+
+  while (quantidade_requisicoes == 0)
+  {
+    char entrada[256];
+
+    printf("%s", "Digite a quantidade de requisições a serem geradas:\n> ");
+
+    // lê uma entrada do usuário
+    fgets(entrada, sizeof(entrada), stdin);
+
+    // se a entrada for inválida ou a quantidade for 0, alerta usuário e pede uma nova entrada
+    if (sscanf(entrada, "%d", &quantidade_requisicoes) != 1 || quantidade_requisicoes == 0)
+    {
+      puts("Entrada inválida, por favor tente novamente.");
+    }
+  }
+
   return 0;
 }
 
@@ -23,6 +60,42 @@ int main(int argc, char const *argv[])
 int get_aleatorio(int min, int max)
 {
   int numero_aleatorio = rand() % (max - min + 1) + min;
-
   return numero_aleatorio;
+}
+
+/**
+ * Coloca PI com a quantidade especificada de dígitos em destino e retorna seu endereço.
+ */
+char *get_pi(char *destino, int digitos)
+{
+  int quantidade_caracteres;
+  char *str;
+
+  if (digitos <= 0)
+  {
+    quantidade_caracteres = 1;
+    str = "3";
+  }
+  else
+  {
+    // '3' + '.' + quantidade de digitos
+    quantidade_caracteres = 2 + digitos;
+    strcat(str, PI);
+  }
+
+  strncpy(destino, str, quantidade_caracteres);
+  destino[quantidade_caracteres] = '\0';
+
+  return destino;
+}
+
+void gera_requisicoes(
+    char *nome_arquivo,
+    int quantidade_requisicoes,
+    int digitos_min,
+    int digitos_max,
+    int tempo_min,
+    int tempo_max)
+{
+  // TODO
 }
