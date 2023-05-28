5bf964841e0e21f17be804edcf375a8b4dcc70f0
Pedro Vinnícius Bernhard <pedro.bernhard@nca.ufma.br>
2023-05-27 21:06:25 -0300

    add: gera requisições

--- main.c
+++ main.c
@@ -2,17 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <limits.h>
 
 // valor de PI com precisão de 100 casas decimais
 const char PI[] = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
 
-int main(int, char **);
-int read_quantidade_requisicoes(int, char **);
-void gera_requisicoes(char *, int, int, int, int, int);
-int get_aleatorio(int, int);
-char *get_pi(char *, int);
+typedef enum bool
+{
+  false = 0,
+  true = 1
+} bool;
 
-int main(int argc, char **argv)
+int main(const int, const char **);
+int read_quantidade_requisicoes(const int, const char **);
+bool gera_requisicoes(const char *, const int, const int, const int, const int, const int);
+
+char *get_pi(char *, const int);
+
+// funções auxiliares
+int get_aleatorio(const int, const int);
+
+// função principal
+int main(const int argc, const char **argv)
 {
   // número de threads trabalhadoras
   const int N = 10;
@@ -20,16 +31,42 @@ int main(int argc, char **argv)
   // periodicidade das requisições em milisegundos
   const int TEMPOREQ = 250;
 
+  // valores dos intervalos
+  const int digitos_min = 10;
+  const int digitos_max = 100;
+  const int tempoespera_min = 500;
+  const int tempoespera_max = 1500;
+
+  // nome do arquivo de requisições
+  const char arquivo_requisicoes[] = "requisicoes.txt";
+
   int quantidade_requisicoes = read_quantidade_requisicoes(argc, argv);
 
-  // TODO: Gerar arquivo de requisições
+  bool requisicoes = gera_requisicoes(
+      arquivo_requisicoes,
+      quantidade_requisicoes,
+      digitos_min,
+      digitos_max,
+      tempoespera_min,
+      tempoespera_max);
+
+  if (!requisicoes)
+  {
+    puts("Erro ao tentar criar o arquivo de requisições!");
+    return 0;
+  }
+
   // TODO: Instanciar threads trabalhadoras
   // TODO: Instanciar thread dispatcher
 
   return 0;
 }
 
-int read_quantidade_requisicoes(int argc, char **argv)
+/**
+ * Lê a quantidade de requisições dos argumentos passados ao programa ou,
+ * caso não tenha sido passado nenhum argumento, lê do input do usuário.
+ */
+int read_quantidade_requisicoes(const int argc, const char **argv)
 {
   int quantidade_requisicoes = 0;
 
@@ -47,9 +84,9 @@ int read_quantidade_requisicoes(int argc, char **argv)
     puts("Nenhum argumento foi passado ao programa.");
   }
 
-  while (quantidade_requisicoes == 0)
+  while (quantidade_requisicoes <= 0)
   {
-    char entrada[256];
+    char entrada[32];
 
     printf("%s", "Digite a quantidade de requisições a serem geradas:\n> ");
 
@@ -57,7 +94,7 @@ int read_quantidade_requisicoes(int argc, char **argv)
     fgets(entrada, sizeof(entrada), stdin);
 
     // se a entrada for inválida ou a quantidade for 0, alerta usuário e pede uma nova entrada
-    if (sscanf(entrada, "%d", &quantidade_requisicoes) != 1 || quantidade_requisicoes == 0)
+    if (sscanf(entrada, "%d", &quantidade_requisicoes) != 1 || quantidade_requisicoes <= 0)
     {
       puts("Entrada inválida, por favor tente novamente.");
     }
@@ -66,30 +103,104 @@ int read_quantidade_requisicoes(int argc, char **argv)
   return quantidade_requisicoes;
 }
 
-void gera_requisicoes(
-    char *nome_arquivo,
-    int quantidade_requisicoes,
-    int digitos_min,
-    int digitos_max,
-    int tempo_min,
-    int tempo_max)
+bool gera_requisicoes(
+    const char *nome_arquivo,
+    const int quantidade_requisicoes,
+    const int digitos_min,
+    const int digitos_max,
+    const int tempo_min,
+    const int tempo_max)
 {
-  // TODO
+  char *txt = calloc(1, sizeof(char));
+  if (txt == NULL)
+  {
+    puts("Erro ao alocar memória para o texto das requisições!");
+
+    // retorna erro
+    return false;
+  }
+
+  // inicializa seed de números aleatório com o tempo atual
+  srand(time(NULL));
+
+  int digitos, tempo;
+
+  // quantidade de caracteres que um inteiro pode ter
+  unsigned long tamanho_int;
+  char str[21];
+  sprintf(str, "%d", INT_MAX);
+  tamanho_int = strlen(str);
+
+  // cria o texto
+  for (int i = 0; i < quantidade_requisicoes; i++)
+  {
+    digitos = get_aleatorio(digitos_min, digitos_max);
+    tempo = get_aleatorio(tempo_min, tempo_max);
+
+    // tamanho do texto
+    unsigned long int tamanho_antigo = strlen(txt) * sizeof(char);
+
+    // tamanho da linha: int + ';' + int + '\n'
+    unsigned long int tamanho_da_linha = tamanho_int + sizeof(char) + tamanho_int + sizeof(char);
+
+    // tamanho antigo + tamanho da linha
+    txt = realloc(txt, tamanho_antigo + tamanho_da_linha);
+
+    // linha: tamanho da linha + '\0'
+    char linha[tamanho_da_linha + sizeof(char)];
+    sprintf(linha, "%d;%d\n", digitos, tempo);
+
+    strcat(txt, linha);
+  }
+
+  // remove último \n
+  char *ultimo_nl = strrchr(txt, '\n');
+  if (ultimo_nl != NULL)
+  {
+    *ultimo_nl = '\0';
+  }
+
+  // cria arquivo
+  FILE *fp = fopen(nome_arquivo, "w");
+  if (fp == NULL)
+  {
+    printf("Erro ao criar arquivo %s!\n", nome_arquivo);
+
+    // retorna erro
+    return false;
+  }
+
+  // escreve no arquivo
+  fprintf(fp, "%s", txt);
+  fclose(fp);
+
+  free(txt);
+
+  // retorna sucesso
+  return true;
 }
 
 /**
  * Retorna um número aleatório no intervalo [min, max].
  */
-int get_aleatorio(int min, int max)
+int get_aleatorio(const int min, const int max)
 {
-  int numero_aleatorio = rand() % (max - min + 1) + min;
+  int min_cpy = min, max_cpy = max;
+  // corrige se ordem estiver invertida
+  if (max_cpy < min_cpy)
+  {
+    int temp = min_cpy;
+    min_cpy = max_cpy;
+    max_cpy = temp;
+  }
+  int numero_aleatorio = rand() % (max_cpy - min_cpy + 1) + min_cpy;
   return numero_aleatorio;
 }
 
 /**
  * Coloca PI com a quantidade especificada de dígitos em destino e retorna seu endereço.
  */
-char *get_pi(char *destino, int digitos)
+char *get_pi(char *destino, const int digitos)
 {
   int quantidade_caracteres;
   char *str;
